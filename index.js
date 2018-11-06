0b97505d0813d70bf3b0f42b1231ba37bef9ceaf
Common <mateus7319@gmail.com>
2018-11-06 19:49:13 -0200

    tt

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const auth_config = config.auth;
 const haru_config = config.haru;
 const activities  = config.activities;
 
-const ajuda = require('./eventos/ajuda.js');
+const eventos = require('./eventos/index.js');
 
 //saber se ela logou mesmo
 harubot.on('ready', () => {
     harubot.user.setActivity(activities.league_of_legends);
-    console.log('logado!');
+    console.log(`${harubot.user.tag} está logada!`);
 })
 
 // interação da bot com o pessoal no canal
 harubot.on('message', message => {
 
   if(message.content === haru_config.prefix + "ajuda") {
-    pedidoAjuda;
+    eventos.ajuda();
   }
 
 
