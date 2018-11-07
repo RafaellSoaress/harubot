4756a5c758f45bb41829fe58f77e8820ce23482c
Common <mateus7319@gmail.com>
2018-11-07 21:24:27 -0200

    Nada relevante

--- config/config.js
+++ config/config.js
@@ -10,9 +10,11 @@ const load_config_file = (filepath) =>
   yaml.load(fs.readFileSync(path.join(__dirname, filepath)));
 
 const general_config = load_config_file('./app_config.yaml');
+const messages_list  = load_config_file('./messages.yaml');
 
 module.exports = {
   auth: general_config.auth,
   haru: general_config.haru,
-  activities: general_config.activities
+  activities: general_config.activities,
+  messages: messages_list
 }
