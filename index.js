47d6ff4b4436626d203d49bf4e02456229f903f1
Saiko <saikodev@outlook.com>
2018-11-07 17:40:05 -0200

    indexando uma chamada de comandos em ajuda

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const {
   activities
 } = require('./config/config.js');
 
+const comandos = require('./config/comandos.json');
+
 const eventos   = require('./eventos/index.js');
 const constants = require('./config/constants.js');
 const utils     = require('./utils/utils.js');
@@ -41,6 +43,7 @@ harubot.on('message', message => {
 
   if(message.content === haru_config.prefix + "ajuda") {
     eventos.ajuda(message);
+    message.content.channel(comandos);
   }
 
 });
