ddeec49c26d8eb326089033778e0d49263ee0aad
Common <mateus7319@gmail.com>
2018-11-07 21:25:34 -0200

    Somente indentação

--- eventos/index.js
+++ eventos/index.js
@@ -1,14 +1,24 @@
+const { messages } = require('../config/config.js');
+
 module.exports = {
 
   ajuda: (message) => {
-    return message.reply('olá, irei passar uma lista de comandos para você !');
+    message.reply(messages.help.channel);
+    message.author.send(messages.help.private);
+
+    return 1;
   },
 
   ready: async (client) => {
     /** Então exibe uma mensagem avisando que o bot foi inizializado com sucesso. */
-    console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`)
+    console.log('log', 'O Bot foi iniciado completamente !')
     /** Configuramos para que apareça "Jogando ..." ou "Playing ..." */
-    client.user.setPresence({ status: 'online', game: { name: process.env.GAME } })
+    client.user.setPresence({
+      status: 'online',
+      game: {
+        name: process.env.GAME
+      }
+    })
   }
 
 }
