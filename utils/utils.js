6305f3b11d247d30da76cb494df80169e69ec527
Common <mateus7319@gmail.com>
2018-11-07 21:26:01 -0200

    Adição de função para buscar os comandos e automaticamente adicionar o prefixo a eles

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,22 @@
+const { haru: haru_config } = require('../config/config.js');
+
+Array.prototype.getPrefix = function() {
+  return this.map(item => haru_config.prefix + item);
+}
+
+String.prototype.getPrefix = function() {
+  return haru_config.prefix + this;
+}
+
 module.exports = {
-  getRandomResponse: (responses) => {
-   return responses[Math.floor(Math.random() * responses.length)];
-  }
+
+  getRandomResponse: (responses) =>
+    responses[Math.floor(Math.random() * responses.length)],
+
+  getListaDeComandos: (comandos) =>
+    Object.keys(comandos),
+
+  getListaDeComandosComPrefixo: (comandos) =>
+    this.getListaDeComandos(comandos).getPrefix()
+
 }
