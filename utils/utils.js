b194588bab30df6b7407c4ce08e1e9a54255e83e
Knevari <mateus7319@gmail.com>
2018-11-07 16:42:28 -0300

    Adicionadas novas funções

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,15 @@
+Array.prototype.getPrefix = function() {
+  return this.map(item => "~" + item);
+}
+
 module.exports = {
+
   getRandomResponse: (responses) => {
    return responses[Math.floor(Math.random() * responses.length)];
+  },
+
+  getListaDeComandos: (comandos) => {
+    return Object.keys(comandos);
   }
+
 }
