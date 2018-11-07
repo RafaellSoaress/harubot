f4bd0acdbee197b35235f122cbccc2456d79e89d
Saiko <saikodev@outlook.com>
2018-11-07 17:20:22 -0200

    testando ?

--- eventos/index.js
+++ eventos/index.js
@@ -11,7 +11,7 @@ module.exports = {
 
   ready: async (client) => {
     /** Então exibe uma mensagem avisando que o bot foi inizializado com sucesso. */
-    console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`)
+    console.log('log', 'O Bot foi iniciado completamente !')
     /** Configuramos para que apareça "Jogando ..." ou "Playing ..." */
     client.user.setPresence({ status: 'online', game: { name: process.env.GAME } })
   }
