e366708267b129ae26ae8724fbf54a90a2c4f74f
Common <mateus7319@gmail.com>
2018-11-07 21:25:36 -0200

    Adição de comentários relevantes para melhor orientação do Código

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+const colors   = require('colors');
 const Discord = require('discord.js');
+
 const harubot = new Discord.Client();
 
-const config  = require('./config/config.js');
-
-const auth_config = config.auth;
-const haru_config = config.haru;
-const activities  = config.activities;
+const {
+  auth: auth_config,
+  haru: haru_config,
+  activities
+} = require('./config/config.js');
 
 const eventos   = require('./eventos/index.js');
 const constants = require('./config/constants.js');
 const utils     = require('./utils/utils.js');
+const comandos  = require('./config/comandos.json');
 
-//saber se ela logou mesmo
+/*
+  Listener aguardando para a compleição
+  dos eventos pré-carregamento.
+*/
+
 harubot.on('ready', () => {
     harubot.user.setActivity(activities.league_of_legends);
-    console.log(`${harubot.user.tag} está logada!`);
+    console.log(`
+
+        ${harubot.user.tag} está logada!
+
+    `.yellow);
 })
 
-// interação da bot com o pessoal no canal
+/*
+  Listener que escuta sempre que uma nova
+  mensagem é enviada no canal
+*/
+
 harubot.on('message', message => {
 
   constants.palavrasOfensivas.forEach(palavra => {
@@ -26,6 +41,11 @@ harubot.on('message', message => {
 
     if(regex.exec(message.content)) {
 
+      /*
+        Retorna uma das mensagens salvas na lista
+        de respostas nas constantes.
+      */
+
       message.reply(utils.getRandomResponse(constants.respostas))
 
       return false;
@@ -33,12 +53,28 @@ harubot.on('message', message => {
     }
   })
 
-  if(message.content === haru_config.prefix + "ajuda") {
-    eventos.ajuda(message);
+  utils.getListaDeComandos(comandos).forEach(comando => {
+    if(comando.getPrefix() === message.content) {
+
+      /*
+        Busca pelo evento relacionado a mensagem
+        através do nome passado como comando   ,
+        caso exista, então executa a respectiva função
+        passando como argumento, o objeto "mensagem"
+        do próprio Discord.
+      */
+
+      if(eventos[comando]) {
+        return eventos[comando](message);
+      }
+
+    }
   }
 
 });
 
-
-//conexao da harubot no canal
+/*
+  Conexão da Harubot no canal utilizando
+  o token como credencial
+*/
 harubot.login(auth_config.token);
